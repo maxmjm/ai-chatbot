1c47bc475723fbc27791b678b6e94f87f72928ea
Maxime Morard <maxmjm@icloud.com>
2025-01-22 12:33:44 +0800

    Use prompt template to query chatbot

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+template = """
+Answer the question below.
+
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer:
+"""
 
 model = OllamaLLM(model="llama3")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
 
-result = model.invoke(input="Hello World")
-
+result = chain.invoke({"context": "", "question": "Hey how are you?"})
 print(result)
